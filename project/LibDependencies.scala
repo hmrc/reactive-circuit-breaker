205d751b06cf22d587f2e2bbf19f1060e45adc38
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-09-03 14:03:57 +0100

    APB-9843: Updates LibDependencies

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object LibDependencies {
 
   val compile = Seq(
-    "ch.qos.logback" % "logback-classic" % "1.5.6",
-    "uk.gov.hmrc"     %% "http-verbs-play-30" % "15.1.0"
+    "uk.gov.hmrc"    %% "http-verbs-play-30" % "15.5.0",
+    "ch.qos.logback" %  "logback-classic"    % "1.5.18"
   )
 
   val test = Seq(
-    "org.scalatest"   %% "scalatest"          % "3.2.19" % Test,
-    "com.vladsch.flexmark"     % "flexmark-all"             % "0.64.8" % Test
-  )
+    "org.scalatest"        %% "scalatest"    % "3.2.19",
+    "com.vladsch.flexmark" %  "flexmark-all" % "0.64.8"
+  ).map(_ % Test)
 }
